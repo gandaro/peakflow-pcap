9b53f5652cd28ae6cdf4d0482019793e810e2f0e
Jakob Kramer <jakob.kramer@gmx.de>
2015-05-17 02:20:59 +0200

    bump version number: bugfix release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pfpcap',
-    version='0.1',
+    version='0.1.1',
     scripts=['pfpcap.py'],
     py_modules=['pfpcap'],
     install_requires=['mechanize>=0.2.5']
